5ffccfd2df557082d28825a7b8c4f7185b971ac4
Lucas Lourenço <kaliware.player@gmail.com>
2022-09-09 14:54:45 -0300

    Project created

---
+++ src/main/java/com/kaliware/integrationviacepjava/IntegrationViacepJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kaliware.integrationviacepjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationViacepJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationViacepJavaApplication.class, args);
+	}
+
+}
